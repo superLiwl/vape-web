ea6fce3d2a6df17206be10fe237c2b969ebe3f5b
superLiwl <lwl.286131719@163.com>
2018-02-02 15:30:37 +0800

    修改配置

---
+++ aldd-vape-web/src/main/java/cn/aldd/vape/constants/CommonConstants.java
@@ -0,0 +1,12 @@
+package cn.aldd.vape.constants;
+
+public class CommonConstants {
+	/**
+	 * 微服务地址
+	 */
+	public static final String MICRO_URL = "http://localhost:9041";
+	/**
+	 * 图片地址
+	 */
+	public static final String IMG_URL = "http://47.95.2.132:9999/images/";
+}
--- aldd-vape-web/src/main/java/cn/aldd/vape/controller/CarouselFigureController.java
+++ aldd-vape-web/src/main/java/cn/aldd/vape/controller/CarouselFigureController.java
@@ -1,12 +1,14 @@
 package cn.aldd.vape.controller;
 
-import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.aldd.vape.constants.CommonConstants;
 import cn.aldd.vape.util.HttpUtils;
+import cn.aldd.vape.util.JSONUtil;
 import cn.aldd.vape.vo.CarouselFigureVo;
 
 @Controller
@@ -14,9 +16,11 @@ public class CarouselFigureController {
 
 	@RequestMapping("/list")
 	public String test(Model model) {
-		List<CarouselFigureVo> list= HttpUtils.sendGet("http://localhost:9041/carouselFigure/micro/1/4",CarouselFigureVo.class);
-		model.addAttribute("list", list);
-		return "carouselFigureList";
+		String result = HttpUtils.sendGet(CommonConstants.MICRO_URL + "/carouselFigure/micro/1/4");
+		Map<String, Object> map = JSONUtil.toMap(result);
+		model.addAttribute("list", JSONUtil.toList(JSONUtil.toJson(map.get("list")), CarouselFigureVo.class));
+		model.addAttribute("imgUrl", CommonConstants.IMG_URL);
+		return "carousel/carouselFigureList";
 	}
 
 }
