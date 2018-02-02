4865c174437dd3f91cf68709c652f64e26278da0
superLiwl <lwl.286131719@163.com>
2018-02-02 11:53:52 +0800

    初始化

---
+++ aldd-vape-web/src/main/java/cn/aldd/vape/alddvapeweb/AlddVapeWebApplication.java
@@ -0,0 +1,12 @@
+package cn.aldd.vape.alddvapeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlddVapeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlddVapeWebApplication.class, args);
+	}
+}
---
+++ aldd-vape-web/src/test/java/cn/aldd/vape/alddvapeweb/AlddVapeWebApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.aldd.vape.alddvapeweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlddVapeWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
